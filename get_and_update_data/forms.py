ca11e58812564b0ab94dd1ea5884681fc99178b8
Victor Boscaro <victorboscaro@outlook.com>
2023-06-02 15:45:35 -0300

    user can type the symbol in the forms

--- get_and_update_data/forms.py
+++ get_and_update_data/forms.py
@@ -2,12 +2,12 @@ from django import forms
 from .models import B3Companie, AssetPrice
 
 class CompanyForm(forms.Form):
-    company = forms.ChoiceField(choices=(), label = "Ativo")
-    start_date = forms.DateField(label = "Data Inicial")
-    end_date = forms.DateField(label = "Data Final")
+    company = forms.CharField(label="Ativo")
+    start_date = forms.DateField(label="Data Inicial")
+    end_date = forms.DateField(label="Data Final")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         unique_values = AssetPrice.objects.values_list('symbol', flat=True).distinct()
         choices = [(value, value) for value in unique_values]
-        self.fields['company'].choices = choices
+        self.fields['company'].widget = forms.Select(choices=choices)
